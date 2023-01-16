3c626452cc876e7b51ef8bb6bb195e7c7602bd37
chienyin <ad_jame@yahoo.com.tw>
2023-01-16 14:37:02 +0800

    update sharing usage of mock

--- src/test/scala/SharingMocks.scala
+++ src/test/scala/SharingMocks.scala
@@ -1,8 +1,9 @@
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.OneInstancePerTest
 
-// 實質共用
-class SharingMocks1 extends AnyFlatSpec with MockFactory {
+// OneInstancePerTest 保證每個測試程式會得到共用物件的 copy, 所以不會互相影響
+class SharingMocks1 extends AnyFlatSpec with MockFactory with OneInstancePerTest {
   val mf = stubFunction[Int, String]
   val n = 123
   val expectRes = "test 123"
@@ -14,13 +15,13 @@ class SharingMocks1 extends AnyFlatSpec with MockFactory {
   }
 
   it should "just use the same stub to test again." in {
-    mf.when(n).returning(expectRes) // why????
     val res = MyObj.fn_cb(n, mf)
     assert(res == expectRes)
   }
 }
 
-// 只是共用程式碼，實際是不同的實體
+// 第二種寫法，共用物件放進trait，再把回傳值改成 new trait
+// 這個寫法在 AsyncXXXSpec 會失敗，因為 AsyncXXXSpec 要求回傳型態必須是 Future[Assertion]，而這個寫法的回傳型態會變成 trait 物件
 class SharingMocks2 extends AnyFlatSpec with MockFactory {
     trait Test { // fixture context
         val mf = stubFunction[Int, String]
